8f279a839ecc54a122299aa61d98051abdef2db8
EdgarTipan <evtipanl1@uce.edu.ec>
2025-04-25 19:41:22 -0500

    Taller 8

--- js/calculadora.js
+++ js/calculadora.js
@@ -55,7 +55,7 @@ const fundamentos = () => {
     const diasLaborables = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'];
     console.log(diasLaborables);
     console.log(diasLaborables[0]);
-    console.log(diasLaborables[10]); /*En JS no existe el null pointer, se reemplaza por umdefined*/
+    console.log(diasLaborables[10]); /*En JS no existe el null pointer, se reemplaza por undefined*/
     let valor = '';
     console.log(valor);
 
@@ -123,8 +123,131 @@ const fundamentos = () => {
         genero: 'Femenino',
         ciudad: 'Manabi'
     }
-
     const estudiantes = [estudiante1, estudiante2, estudiante3];
     console.log(estudiantes);
     console.table(estudiantes);
+
+    const ciudadano = {
+        nombre: 'Edgar',
+        apellido: 'Tipan',
+        direccion: {
+            callePrincipal: 'Av. America',
+            calleSecundaria: 'Av. La Gasca',
+            numeracion: '5620',
+            barrio: {
+                referencia: 'Frente al Dilipa'
+            }
+        }
+    }
+    console.log(ciudadano);
+    console.log(ciudadano.nombre);
+    console.log(ciudadano.direccion);
+    console.log(ciudadano.direccion.callePrincipal);
+    ciudadano.direccion.callePrincipal = 'Av. Amazonas';
+    console.log(ciudadano);
+
+    const estudiantes2 = [{ nombre: 'Carla', apellido: 'Castillo' }, { nombre: 'Viviana', apellido: 'Paez' }];
+    console.log(estudiantes2);
+    console.log(estudiantes2[1].apellido);
+
+    /*Desestructuración de arreglos*/
+    const colores = ['Amarillo', 'Azul', 'Rojo', 'Verde', 'Naranja'];
+    const [c1, c2, c3, c4, c5] = colores; /*Desestructuramos otorgando un alias*/
+    console.log(c1);
+    console.log(c5);
+
+    const [cuno, cdos] = colores;
+    console.log(cuno);
+    console.log(cdos);
+
+    const [, , ctres] = colores;
+    console.log(ctres);
+
+    const [c01, c02] = ['Amarillo', 'Azul', 'Rojo', 'Verde', 'Naranja'];
+
+    desestructuracionArreglo(colores); /*Se puede desestructurar cualquier variable que se sepa que es un arreglo*/
+
+    const [, p2, p3, p4, p5, p6, p7, p8, p9, p10] = desestructuracionArreglo2();
+    console.log(p2);
+    console.log(p3);
+    console.log(p10);/*Conclusion: La desestructuracion de un objeto se la hace a traves de sus posiciones*/
+
+    /*Desestructuracion por operador REST:
+    Desestructura un arreglo, eligiendo lo que queremos conservar*/
+    console.log('Desestructuracion por operador REST:')
+    const[p1,...resto] = desestructuracionArreglo2();
+    console.log(p1);
+    console.log(resto);
+
+    /*Desestructuración de objetos*/
+    const auto = {
+        marca: 'Toyota',
+        modelo: 'Prius',
+        anio: 2020,
+        color: 'Amarillo'
+    }
+
+    const { marca, color, anio } = auto; /*las posiciones no importan*/
+    console.log(color);
+
+    const { anio1 } = {
+        marca1: 'Toyota',
+        modelo1: 'Prius',
+        anio1: 2020,
+        color1: 'Amarillo'
+    }
+    console.log(anio1);
+
+    desestructuracionObjeto(auto);/*Se pueden desestructurar objetos que entran como variables*/
+
+    const universidad = {
+        nombre: 'UCE',
+        direccion: 'America',
+        rector: {
+            nombreR: 'Daniel',
+            apellidoR: 'Teran'
+        }
+    }
+    const { rector, nombreR } = universidad;
+    console.log(rector);
+    const { apellidoR } = rector;
+    console.log(apellidoR);
+
+    const universidad2 = {
+        nombre2: 'UCE',
+        direccion2: 'America',
+        rector2: {
+            nombreR2: 'Daniel',
+            apellidoR2: 'Teran'
+        }
+    }
+    const { nombre2, rector2: { nombreR2 } } = universidad2;
+    console.log(nombreR2);
+
+    /*Desestructuracion por operador REST para objetos:*/
+    const autoR = {
+        marcaR: 'Toyota',
+        modeloR: 'Prius',
+        anioR: 2020,
+        colorR: 'Amarillo'
+    }
+    const{marcaR,...restoO} = autoR;
+    console.log(marcaR);
+    console.log(restoO)
+}
+
+function desestructuracionArreglo([c1, c2, c3]) {
+    console.log(c1);
+    console.log(c2);
+    console.log(c3);
+}
+
+function desestructuracionArreglo2() {
+    const colores = ['Rosado', 'Azul', 'Rojo', 'Verde', 'Naranja'];
+    return colores;
+}
+
+function desestructuracionObjeto({ marca, color }) {
+    console.log(marca);
+    console.log(color);
 }
